c0bd616860f832e7857ab296d768ed8431bf7adb
etalmacsi <eddyedvin@gmail.com>
2019-04-06 18:16:02 +0200

    Deploy

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
